07d162f0be9b06bd3776652eef6b02e4790e5ec3
AshZeng1 <xuesong.zeng@thoughtworks.com>
2023-12-12 14:34:45 +0800

    Avatars and name change

--- Main.py
+++ Main.py
@@ -4,28 +4,47 @@ import string
 from PIL import Image
 import os
 
+
 class Background(Enum):
-    STAR = {'value':'star','image_path':  os.path.abspath('./img/Background/01.png')}
-    GALAXY = {'value':'galaxy','image_path': os.path.abspath('./img/Background/02.png')}
-    NEWWORLD = {'value':'newWorld','image_path': os.path.abspath('./img/Background/03.png')}
-    NEXTDAY = {'value':'nextDay','image_path': os.path.abspath('./img/Background/04.png')}
-    CLOUD = {'value':'cloud','image_path': os.path.abspath('./img/Background/05.png')}
-    MOUNTAIN = {'value':'mountain','image_path': os.path.abspath('./img/Background/06.png')}
+    STAR = {"value": "Star", "image_path": os.path.abspath("./img/Background/01.png")}
+    GALAXY = {
+        "value": "Galaxy",
+        "image_path": os.path.abspath("./img/Background/02.png"),
+    }
+    NEWWORLD = {
+        "value": "New World",
+        "image_path": os.path.abspath("./img/Background/03.png"),
+    }
+    NEXTDAY = {
+        "value": "Next Day",
+        "image_path": os.path.abspath("./img/Background/04.png"),
+    }
+    CLOUD = {"value": "Cloud", "image_path": os.path.abspath("./img/Background/05.png")}
+    MOUNTAIN = {
+        "value": "Mountain",
+        "image_path": os.path.abspath("./img/Background/06.png"),
+    }
+
 
 class Animals(Enum):
-    BEAR = {'value':'bear','image_path': os.path.abspath('./img/Animals/01.png')}
-    LEO = {'value':'leo','image_path': os.path.abspath('./img/Animals/02.png')}
-    CAT = {'value':'cat','image_path': os.path.abspath('./img/Animals/03.png')}
-    GRAFF = {'value':'graff','image_path': os.path.abspath('./img/Animals/04.png')}
-    ROCC = {'value':'rocc','image_path': os.path.abspath('./img/Animals/05.png')}
-    
+    BEAR = {"value": "Bear", "image_path": os.path.abspath("./img/Animals/01.png")}
+    LEO = {"value": "Leo", "image_path": os.path.abspath("./img/Animals/02.png")}
+    CAT = {"value": "Cat", "image_path": os.path.abspath("./img/Animals/03.png")}
+    GRAFF = {"value": "Graff", "image_path": os.path.abspath("./img/Animals/04.png")}
+    ROCC = {"value": "Rocc", "image_path": os.path.abspath("./img/Animals/05.png")}
+
+
 class Wearing(Enum):
-    FLASH = {'value':'falsh','image_path': os.path.abspath('./img/Wearing/01.png')}
-    PETS = {'value':'ptes','image_path': os.path.abspath('./img/Wearing/02.png')}
-    TREE = {'value':'tree','image_path': os.path.abspath('./img/Wearing/03.png')}
-    VEG = {'value':'veg','image_path': os.path.abspath('./img/Wearing/04.png')}
-    STAR = {'value':'star','image_path': os.path.abspath('./img/Wearing/05.png')}
-    RAINBOW = {'value':'rainbow','image_path': os.path.abspath('./img/Wearing/06.png')}
+    FLASH = {"value": "Flash", "image_path": os.path.abspath("./img/Wearing/01.png")}
+    PETS = {"value": "Pets", "image_path": os.path.abspath("./img/Wearing/02.png")}
+    TREE = {"value": "Tree", "image_path": os.path.abspath("./img/Wearing/03.png")}
+    VEG = {"value": "Veg", "image_path": os.path.abspath("./img/Wearing/04.png")}
+    STAR = {"value": "Star", "image_path": os.path.abspath("./img/Wearing/05.png")}
+    RAINBOW = {
+        "value": "Rainbow",
+        "image_path": os.path.abspath("./img/Wearing/06.png"),
+    }
+
 
 class Student:
     def __init__(self, stu_id, name, background, animals, wearing, avatar):
@@ -40,9 +59,10 @@ class Student:
         return (
             f"Student(id={self.stu_id}, name={self.name}, "
             f"background={self.background.value}, animals={self.animals.value}, "
-            f"wearing={self.wearing.value})," 
+            f"wearing={self.wearing.value}),"
         )
-       
+
+
 class Classroom:
     def __init__(self):
         self.students = []
@@ -50,17 +70,11 @@ class Classroom:
     def add_student(self, student):
         self.students.append(student)
 
-    def find_student_by_attributes(self, background, animals, wearing):
-        for student in self.students:
-            if (
-                student.background == background
-                and student.animals == animals
-                and student.wearing == wearing 
-            ):
-                return student
-            return None
 
-def generate_avatar(background_path, animal_path, wearing_path, output_path):
+# -------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+def generate_avatar(background_path, animal_path, wearing_path, output_folder, name):
     # 打开三张图片
     background_image = Image.open(background_path).convert("RGBA")
     animal_image = Image.open(animal_path).convert("RGBA")
@@ -79,17 +93,34 @@ def generate_avatar(background_path, animal_path, wearing_path, output_path):
     new_image = Image.new("RGBA", (new_width, new_height), (255, 255, 255, 0))
 
     # 将三张图片堆叠到新图片上
-    new_image.paste(background_image, ((new_width - bg_width) // 2, (new_height - bg_height) // 2), background_image)
-    new_image.paste(animal_image, ((new_width - ani_width) // 2, (new_height - ani_height) // 2), animal_image)
-    new_image.paste(wearing_image, ((new_width - wear_width) // 2, (new_height - wear_height) // 2), wearing_image)
+    new_image.paste(
+        background_image,
+        ((new_width - bg_width) // 2, (new_height - bg_height) // 2),
+        background_image,
+    )
+    new_image.paste(
+        animal_image,
+        ((new_width - ani_width) // 2, (new_height - ani_height) // 2),
+        animal_image,
+    )
+    new_image.paste(
+        wearing_image,
+        ((new_width - wear_width) // 2, (new_height - wear_height) // 2),
+        wearing_image,
+    )
 
     # 合并图片
-    final_image = Image.alpha_composite(Image.new("RGBA", new_image.size, (255, 255, 255, 255)), new_image)
+    final_image = Image.alpha_composite(
+        Image.new("RGBA", new_image.size, (255, 255, 255, 255)), new_image
+    )
 
     # 保存新图片
+    os.makedirs(output_folder, exist_ok=True)
+    output_path = f"{output_folder}/{name}.png"
     final_image.save(output_path)
-        
-def generate_random_student(existing_students):
+
+
+def generate_random_student(existing_students, output_folder):
     stu_id = f"A{random.randint(100, 999)}"
     background = random.choice(list(Background))
     animals = random.choice(list(Animals))
@@ -98,22 +129,74 @@ def generate_random_student(existing_students):
     existing_students["backgrounds"].append(background)
     existing_students["animals"].append(animals)
     existing_students["wearings"].append(wearing)
-    
-    avatar_output_path = f"avatar_{stu_id}.png"
-    generate_avatar(background.value["image_path"], animals.value["image_path"], wearing.value["image_path"], avatar_output_path)
-    avatar = avatar_output_path
-   
-    name = f"{background} {animals} {wearing}"
+    name = (
+        f"{background.value['value']} {wearing.value['value']} {animals.value['value']}"
+    )
 
+    avatar_output_path = f"{output_folder}"
+
+    generate_avatar(
+        background.value["image_path"],
+        animals.value["image_path"],
+        wearing.value["image_path"],
+        avatar_output_path,
+        name,
+    )
+    avatar = f"{output_folder}/{name}.png"
     return Student(stu_id, name, background, animals, wearing, avatar)
+
+
+def find_student(students, background, animals, wearing):
+    # 检查输入的属性是否合法
+    if background not in [background.value["value"] for background in Background]:
+        print(f"Error: Invalid background value '{background}'.")
+        return
+    if animals not in [animals.value["value"] for animals in Animals]:
+        print(f"Error: Invalid animals value '{animals}'.")
+        return
+    if wearing not in [wearing.value["value"] for wearing in Wearing]:
+        print(f"Error: Invalid wearing value '{wearing}'.")
+        return
+
+    # 查找学生
+    matching_students = [
+        student
+        for student in students
+        if student.background.value["value"] == background
+        and student.animals.value["value"] == animals
+        and student.wearing.value["value"] == wearing
+    ]
+
+    if not matching_students:
+        print(
+            f"No student found with background '{background}', animals '{animals}', wearing '{wearing}'."
+        )
+        return
+
+    # 返回找到的学生信息
+    for student in matching_students:
+        print(
+            f"Student ID: {student.student_id}, Name: {student.name}, Avatar: {student.avatar}"
+        )
+
 
 if __name__ == "__main__":
     classroom = Classroom()
     existing_students = {"backgrounds": [], "animals": [], "wearings": []}
+    output_folder = os.path.abspath("./img/Avatar")
 
-    for _ in range(24):
-        random_student = generate_random_student(existing_students)
-        classroom.add_student(random_student)
-    
+    # for _ in range(24):
+    #     random_student = generate_random_student(existing_students, output_folder)
+    #     classroom.add_student(random_student)
+
     for student in classroom.students:
         print(student)
+        
+        
+    # background_to_find = "Cloud"
+    # wearing_to_find = "Flash"
+    # animals_to_find = "Graff"
+
+    # find_student(
+    #     classroom.students, background_to_find, animals_to_find, wearing_to_find
+    # )
